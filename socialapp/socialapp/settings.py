3d6926f6574edf31fbc35ab36be3fbc81dca47f4
Habib Ur Rehman <x24170658@student.ncirl.ie>
2025-04-06 01:40:29 +0100

    Update settings.py

--- socialapp/socialapp/settings.py
+++ socialapp/socialapp/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['socialappproj-env.eba-gbuciki6.us-east-1.elasticbeanstalk.com'
     '127.0.0.1',
     'localhost',
     '172.31.86.222',
-    '3.83.29.195',]           ]
+    '3.83.29.195',] # Add your allowed hosts here
 
 
 # Application definition
